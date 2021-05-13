742278e078ae8591a8138357fbe98bfb996c9e37
IT1810 <7cezar97@gmail.com>
2021-05-13 20:29:15 +0200

    04 - dodelane zakladni templaty

--- zakaznici/urls.py
+++ zakaznici/urls.py
@@ -3,4 +3,5 @@ from zakaznici import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('zakaznici/', views.ZakaznikListView.as_view(), name='prehled_zakazniku'),
+    path('zakaznici/<int:pk>/', views.ZakaznikDetailView.as_view(), name='podrobnosti_zakaznika')
 ]
--- zakaznici/views.py
+++ zakaznici/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from zakaznici.models import *
 
 
@@ -9,12 +9,21 @@ from zakaznici.models import *
 def index(requests):
 
     num_customers = Zakaznik.objects.all().count()
+    num_tarify = Tarif.objects.all().count()
+    num_sluzby = DruhotneSluzby.objects.all().count()
     customers = Zakaznik.objects.order_by('prijmeni')[:5]
     contracts = Smlouva.objects.order_by('-zakaznik')
+    tarify = Tarif.objects.all()
+    sluzby = DruhotneSluzby.objects.all()
+    num_sluzby
     context = {
         'num_customers': num_customers,
+        'num_tarify': num_tarify,
+        'num_sluzby': num_sluzby,
         'customers': customers,
         'contracts': contracts,
+        'sluzby': sluzby,
+        'tarify': tarify,
     }
     return render(requests, 'index.html', context=context)
 
@@ -25,3 +34,9 @@ class ZakaznikListView(ListView):
     extra_context = {'contracts': Smlouva.objects.all(),
                      'num_customers': Zakaznik.objects.all().count()}
     template_name = 'zakaznik/prehled.html'
+
+class ZakaznikDetailView(DetailView):
+    model = Zakaznik
+    context_object_name = 'podrobnosti_zakaznika'
+    template_name = 'zakaznik/podrobnosti.html'
+    extra_context = {'contracts': Smlouva.objects.all()}
